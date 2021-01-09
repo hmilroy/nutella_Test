7f7bc5f383625596be3431b70b090d755cb28cf5
hmilroy <h.milroyperera@gmail.com>
2021-01-09 23:16:01 +0530

    data loading using json

--- tests/andi/create_property.spec.js
+++ tests/andi/create_property.spec.js
@@ -1,6 +1,8 @@
 //const { describe } = require("mocha");
+describe("Different Test suite",function(){
 
-describe("Different Test suite", () => {
+
+
   const map = {
     create_property_button:"[class='wtml-button wtml-button--warhead wtml-button--medium wtml-button--ghost topbar__actions_item']",
     address_type:"#differentApp > div > div.content-dock > div > form > div.inner-form.add-property-form > div.custom-address-comp > div:nth-child(1) > div > div > select",
@@ -10,7 +12,16 @@ describe("Different Test suite", () => {
     add_property_save_button: "[class='button button_main']"
   };
 
-  it("Test Create Property flow", () => {
+  beforeEach("Load", function()
+  {
+      cy.fixture("createpropertydata").then((data)=>{
+          this.key = data
+          
+        })
+  })
+
+
+  it("Test Create Property flow", function(){
     cy.visit("https://admin-qa.different.com.au/#/");
     cy.contains("Sign in with Google", { timeout: 10000 }).click();
     cy.location("protocol").should("eq", "https:");
@@ -21,18 +32,18 @@ describe("Different Test suite", () => {
     cy.get("[class='MA-cantFind']").click();
     cy.get(map.address_type, { timeout: 10000 }).select("Suite");
     cy.get(map.region).select("Queensland");
-
+      
     cy.get("form").within(($form) => {
-      cy.get("input").first().type("101");
-      cy.get("input").eq(1).type("88");
-      cy.get("input").eq(2).type("47");
-      cy.get("input").eq(3).type("Cypress automation property");
+      cy.get("input").first().type(this.key.number1);
+      cy.get("input").eq(1).type(this.key.number2);
+      cy.get("input").eq(2).type(this.key.number3);
+      cy.get("input").eq(3).type(this.key.property_name);
       cy.get("[name='streetType']").select("Road");
-      cy.get("input").eq(4).type("Cypress suburb");
-      cy.get("input").eq(5).type("5008");
-      cy.get("input").eq(6).type("h.milroyperera+press02@gmail.com");
+      cy.get("input").eq(4).type(this.key.suburb);
+      cy.get("input").eq(5).type(this.key.postcode);
+      cy.get("input").eq(6).type(this.key.owner_email);
       cy.get(map.add_new_email).click();
-      cy.get(map.owner_name).type("Auto Owner 02");
+      cy.get(map.owner_name).type(this.key.owner_name1);
       cy.get(map.add_property_save_button).click();
     });
   });
